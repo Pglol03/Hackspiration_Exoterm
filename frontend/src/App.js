84798d5677aee5108831b95f1de2e2f12f51d5a5
Pradyun Gayakwad <pradyungayakwad@gmail.com>
2023-02-25 11:03:22 +0530

    Connection Speed hand heatmap done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Heatmap from './components/Map'
+import ConnectionSpeed from './components/ConnectionSpeed'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <ConnectionSpeed/>
+      <Heatmap/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ frontend/src/components/Map.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// d13ae3c193ea30b892ebe60ac054a932
+
+export default function Heatmap() {
+
+
+    return (
+        <div>
+            <button></button>
+        </div>
+    )
+}
