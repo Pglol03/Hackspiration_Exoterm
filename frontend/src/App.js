7a26de8594c95deeae961f2152df3862b41884e2
Pradyun Gayakwad <pradyungayakwad@gmail.com>
2023-02-25 12:22:16 +0530

    navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Heatmap from './components/Map'
 import ConnectionSpeed from './components/ConnectionSpeed'
+import Navbar from './components/Navbar'
+
 
 const App = () => {
   return (
     <div className='container'>
+      <Navbar/>
       <ConnectionSpeed/>
       <Heatmap/>
     </div>
