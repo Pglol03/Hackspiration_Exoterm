0f94fe4a66ae4a4298385efe25a9bce473e2be93
Pradyun Gayakwad <pradyungayakwad@gmail.com>
2023-02-25 11:58:52 +0530

    Map function  added

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Helmet } from "react-helmet"
+function iframe() {
+    return {
+        __html: '<iframe src="./Heatmap.html" width="540" height="450"></iframe>'
+    }
+}
 
 // d13ae3c193ea30b892ebe60ac054a932
 
 export default function Heatmap() {
-
-
+   
     return (
         <div>
-            <button></button>
-        </div>
+        <div dangerouslySetInnerHTML={iframe()} />
+    </div>
     )
 }
